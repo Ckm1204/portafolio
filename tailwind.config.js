4b0fe9875ae1420282e0952fcc4e60c51913ccaf
“Camilo” <camilotello04@gmail.com>
2023-10-04 19:35:23 -0500

    arreglos

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto','italic'], oswald: ['Roboto','oswald']
+      },
+      
+    },
   },
   plugins: [],
 }
-
